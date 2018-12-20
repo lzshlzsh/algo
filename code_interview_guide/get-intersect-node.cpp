1ee2e947626c3a86fb4a18799352637eb507163a
lzshlzsh <lzshlzsh@163.com>
2018-12-20 09:56:21 +0800

    get-intersect-node

--- code_interview_guide/get-intersect-node.cpp
+++ code_interview_guide/get-intersect-node.cpp
@@ -21,6 +21,11 @@ int get_intersect_node(Node *link1, Node *link2) {
     return 0;
   }
 
+  if (link1 == link2) {
+    std::cout << "inersect node: " << link1->value_ << std::endl;
+    return 0;
+  }
+
   auto first_loop_node1 = get_first_loop_node(link1);
   auto first_loop_node2 = get_first_loop_node(link2);
   if ((first_loop_node1 && !first_loop_node2) ||
@@ -57,6 +62,38 @@ int get_intersect_node(Node *link1, Node *link2) {
       std::cout << "no intersect" << std::endl;
     }
   } else {
+    int n = 0;
+    Node *p1, *p2;
+
+    for (p1 = first_loop_node1;
+         p1->next_ != first_loop_node1 && p1 != first_loop_node2;
+         p1 = p1->next_); 
+
+    if (p1 != first_loop_node2) {
+      std::cout << "no intersect" << std::endl;
+      return 0;
+    }
+
+    if (first_loop_node1 != first_loop_node2) {
+      std::cout << "inersect node: " << first_loop_node1->value_ 
+       << " or " << first_loop_node2->value_ << std::endl;
+      return 0;
+    }
+
+    for (p1 = link1; p1 != first_loop_node1; n++, p1 = p1->next_);
+    for (p2 = link2; p2 != first_loop_node2; n--, p2 = p2->next_);
+    if (n < 0) {
+      n = -n;
+      p1 = link2;
+      p2 = link1;
+    } else {
+      p1 = link1;
+      p2 = link2;
+    }
+
+    for (; n--; p1 = p1->next_);
+    for (; p1 != p2; p1 = p1->next_, p2 = p2->next_);
+    std::cout << "inersect node: " << p1->value_ << std::endl;
   }
 
   return 0;
@@ -95,7 +132,7 @@ int test_case2() {
 
   std::cout << "两个有环链表交于环上相同点" << std::endl;
   auto tmp1 = link1;
-  while (tmp1->value_ != 105) {
+  while (tmp1->value_ != 103) {
     tmp1 = tmp1->next_;
   }
 
