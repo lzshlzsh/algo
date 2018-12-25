2317555ff8400d83b7ef5b8413bc99aa7b63d245
lzshlzsh <lzshlzsh@163.com>
2018-12-25 23:09:04 +0800

    longest_common_substr

--- longest_common_substr.cpp
+++ longest_common_substr.cpp
@@ -9,11 +9,46 @@
 #include <time.h>
 
 #include <iostream>
+#include <vector>
 
 #include "string_common.h"
 
 using namespace common;
 namespace {
+
+int lc_substr(const std::string &str1, const std::string &str2) {
+  auto const len1 = str1.length();
+  auto const len2 = str2.length();
+  std::vector<std::vector<int>> lc;
+  int lc_max = 0;
+  size_t pos = 0;
+  
+  lc.resize(len1);
+  for (auto &it: lc) {
+    it.resize(len2);
+  }
+
+  for (auto i = 0; i < len1; ++i) {
+    for (auto j = 0; j < len2; ++j) {
+      if (i == 0 || j == 0) {
+        lc[i][j] = 0;
+      } else if (str1[i-1] == str2[j-1]) {
+        lc[i][j] = lc[i-1][j-1] + 1;
+        if (lc[i][j] > lc_max) {
+          lc_max = lc[i][j];
+          pos = i-1;
+        }
+      } else {
+        lc[i][j] = 0;
+      }
+    }
+  }
+
+  std::cout << lc_max << ':' << str1.substr(pos - lc_max + 1, lc_max)
+    << std::endl;
+
+  return 0;
+}
 
 int run(const size_t len) {
   std::string str, str2;
@@ -24,6 +59,8 @@ int run(const size_t len) {
 
   std::cout << str << std::endl;
   std::cout << str2 << std::endl;
+ 
+  lc_substr(str, str2);
   return 0;
 } 
 
